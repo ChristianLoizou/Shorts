261c330dcdd652db088de6643ef28ff8c997c2c9
Christian Loizou <cloizou777@gmail.com>
2021-03-11 11:05:05 +0000

    Added options menu for selecting parallels to detect

--- parallelchecker/main.py
+++ parallelchecker/main.py
@@ -4,7 +4,7 @@ import xml.etree.ElementTree as ElementTree
 from itertools import combinations
 from tkinter import *
 from tkinter.filedialog import askopenfilename as openfilename
-from tkinter.ttk import Button, Frame, Label
+from tkinter.ttk import Button, Checkbutton, Frame, Label, LabelFrame, Style
 
 
 
@@ -13,6 +13,7 @@ class Application:
         self.width, self.height = width, height
         self.root = Tk()
         self.root.title("Parallel Checker")
+        self.root.geometry(f"{width}x{height}")
         self.root.resizable(False, False)
         self.version = 0.1
         self.setup_window()
@@ -20,25 +21,46 @@ class Application:
     def setup_window(self):
         self.mainframe = Frame(self.root, width=self.width, height=self.height)
         self.output_text_variable = StringVar(value='Open a MusicXML file to check it for parallels!')
-        out_lbl = Label(self.mainframe, textvariable=self.output_text_variable, width=40)
-        load_btn = Button(self.mainframe, text="Load .musicxml file", command=self.load_file, width=40)
-        sett_btn = Button(self.mainframe, text="Options", command=self.options_menu, width=40)
+        out_lbl = Label(self.mainframe, textvariable=self.output_text_variable, width=self.width/5)
+        load_btn = Button(self.mainframe, text="Load .musicxml file", command=self.load_file, width=self.width/5)
+        sett_btn = Button(self.mainframe, text="Options", command=self.options_menu, width=self.width/5)
 
         out_lbl.grid(row=0, column=0, columnspan=3, sticky="nsew")
-        load_btn.grid(row=1, column=0, sticky="nsew")
-        sett_btn.grid(row=2, column=0, sticky="nsew")
-        self.mainframe.pack(fill='both', expand=True, padx=15, pady=15)
+        load_btn.grid(row=1, column=0, columnspan=3, sticky="nsew")
+        sett_btn.grid(row=2, column=0, columnspan=3, sticky="nsew")
+        self.mainframe.place(anchor='c', relx=.5, rely=.5)
     
     def run(self):
         self.root.mainloop()
     
-    def options_menu(self, _=None):
-        print("Options button clicked")
+    def options_menu(self, width=250, height=450):
+        settings_styles = Style()
+        settings_styles.configure("SettingsLabelframe.TLabelframe.Label", font=("Verdana", 15, "normal"))
+        settings_styles.configure("SettingsLabelframe.TLabelframe.Label", foreground='black')
+
+        popup = Toplevel(self.root)
+        popup.title("Options")
+        popup.resizable(False, False)
+        popup.geometry(f"{width}x{height}")
+        mainframe = Frame(popup, width=width, height=height)
+        int_frame = LabelFrame(mainframe, text="Parallels to detect", style="SettingsLabelframe.TLabelframe")
+        note_lbl = Label(mainframe, text="Note: these will also detect compound\nversions of themselves \n(ie. Compound Perfect 5th)")
+        for i, i_n in enumerate(interval_names):
+            cb = Checkbutton(int_frame, text=i_n, variable=interval_variables[i])
+            cb.pack(anchor='w')
+        all_btn = Button(int_frame, text="Toggle all", command=all_intervals)
+        all_btn.pack()
+        int_frame.pack(padx=15, pady=15)
+        note_lbl.pack(pady=5)
+        mainframe.pack()
+        popup.mainloop()
     
     def load_file(self, _=None):
         fn = openfilename(filetypes=[("Uncompressed MusicXML files", '*.musicxml')], title="Load an Uncompressed MusicXML file")
         results = execute_parallel_check(fn)
-        self.output_text_variable.set('\n'.join([f"Parallel {i} detected at beat {b} between parts {' and '.join(p)}" for (i, b, p) in results]))
+        results = filter(lambda t: interval_variables[interval_names.index(convert_interval_to_name(t[0]).replace("Compound ", ''))].get()==1, results)
+        lines = [f"Parallel {convert_interval_to_name(i)} detected at beat {b} between parts {' and '.join(p)}" for (i, b, p) in results]
+        self.output_text_variable.set('\n'.join(lines))
         return
 
 
@@ -105,15 +127,6 @@ class CustomExceptionTypes:
 def get_data_dict(file_name):
     music_dict = dict()
     xml = ElementTree.parse(file_name).getroot()
-    
-    # for part in xml.part:
-    #     music_dict[part['id']] = dict()
-    #     for i, measure in enumerate(part.measure):
-    #         if i == 0:
-    #             key = int(measure.attributes.key.fifths.cdata)
-    #         music_dict[part['id']][f"b{measure['number']}"] = list()
-    #         for note in measure.note:
-    #             music_dict[part['id']][f"b{measure['number']}"].append(Note(note, key))
 
     for part in xml.findall('part'):
         part_id = part.attrib['id']
@@ -143,7 +156,6 @@ def convert_to_smallest_note_value(data):
                     )
     return new_data
 
-
 def check_for_parallels(data):
     try:
         results = list()
@@ -171,14 +183,35 @@ def check_for_parallels(data):
     except CustomExceptionTypes.InequalPartLengths as e:
         print(f"{type(e)}: {e}")
 
+def convert_interval_to_name(interval):
+    if interval > len(interval_names):
+        return f"Compound {interval_names[interval%12]}"
+    return interval_names[interval]
+
 def execute_parallel_check(file_path, _=None):
     data = get_data_dict(file_path)
     data = convert_to_smallest_note_value(data)
     results = check_for_parallels(data)
     return results
 
+def all_intervals():
+    global toggle
+    for i in interval_variables:
+        i.set(toggle)
+    toggle = int(not toggle)
+
 if __name__ == "__main__":
     application = Application()
+    interval_names = [
+        "Perfect Unison", "Minor 2nd",
+        "Major 2nd", "Minor 3rd",
+        "Major 3rd", "Perfect 4th",
+        "Augmented 4th / Diminshed 5th", "Perfect 5th",
+        "Minor 6th", "Major 6th", "Minor 7th",
+        "Major 7th", "Perfect Octave"
+    ]
+    toggle = 0
+    interval_variables = [IntVar(value=1) for _ in range(len(interval_names))]
     note_names = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
     pitches = [f"{note}{octave}" for octave in range(-1, 10) for note in note_names][:-4] # C-1 - G9
     keys = dict()
