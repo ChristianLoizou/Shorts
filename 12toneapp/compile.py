3f9722532423d70c799e9f93e48661b7f69fbfae
ChristianLoizou <cloizou777@gmail.com>
2021-03-24 08:37:41 +0000

    Added support for compilation to Darwin .app

--- 12toneapp/compile.py
+++ 12toneapp/compile.py
@@ -1,6 +1,8 @@
 from os import chdir, listdir, remove, system
 from shutil import copyfile as copy
 from shutil import rmtree
+from sys import platform
+from time import sleep
 
 PROJECT_NAME = "12toneapp"
 
@@ -23,12 +25,31 @@ with open("VERSION_DATA", 'w') as version_data_file:
             version_data_file.write(':'.join(l) + "\n")
 
 chdir(PROJECT_NAME)
-copy('main.py', f'pys\\v{curr_version}.py')
-if 'latest.py' in listdir():
-    copy('latest.exe', f'exes\\v{last_version}.exe')
-system("pyinstaller --onefile -w main.py")
-copy('dist\\main.exe', 'latest.exe')
-copy('latest.exe', f'exes\\v{curr_version}.exe')
-for directory in ["dist", "build", "__pycache__"]:
-    rmtree(directory)
-remove('main.spec')
+if platform == 'win32':
+    copy('main.py', f'pys\\v{curr_version}.py')
+    if 'latest.py' in listdir():
+        copy('latest.exe', f'exes\\v{last_version}.exe')
+    system("pyinstaller --onefile -w main.py")
+    copy('dist\\main.exe', 'latest.exe')
+    copy('latest.exe', f'exes\\v{curr_version}.exe')
+    for directory in ["dist", "build", "__pycache__"]:
+        rmtree(directory)
+    remove('main.spec')
+elif platform == 'darwin':
+    DEPENDENCIES = ['assets', 'application_update.py']
+    system('py2applet --make-setup main.py')
+    with open('setup.py', 'r') as setup_file:
+        setup_lines = setup_file.readlines()
+    with open('setup.py', 'w') as setup_file:
+        for line in setup_lines:
+            if line.startswith('DATA_FILES = '):
+                line = line.replace("[]", repr(DEPENDENCIES))
+            setup_file.write(line)
+    system('python3.7 setup.py py2app')
+    sleep(.5)
+    system('cp -r dist/main.app/ latest.app/')
+    system(f'cp -r dist/main.app/ apps/v{curr_version}.app/')
+    for directory in ['dist', 'build']:
+        rmtree(directory)
+    remove('setup.py')
+
