cb2b89927cb45dc95d8bba086eb397ed9aadf66b
ChristianLoizou <cloizou777@gmail.com>
2021-03-26 19:27:40 +0000

    Created basic application layout and resizable cell grid

--- musicmatrix/main.py
+++ musicmatrix/main.py
@@ -1,21 +1,29 @@
+import numpy as np
 from os import path, sep
+from random import choice as randchoice
 from sys import exit, platform
 from tkinter import *
 from tkinter import font
-from tkinter.ttk import Label, LabelFrame
+from tkinter.ttk import Label, LabelFrame, Button as ttkButton
 from functools import partial
 
+class Cell:
+    def __init__(self, content='_'):
+        self.content = content
+    
+    def __repr__(self):
+        return f"<Cell object - content: {self.content}>"
+
 class Application:
-    def __init__(self, version, width=800, height=800):
+    def __init__(self, version, width=800, height=900):
         self.window = Tk()
         self.version = version
         self._width, self._height = width, height
-        self._frame_pad = 15
+        self._frame_pad = 25
         self._minimum_gridsize = 2
         self._maximum_gridsize = 20
         self._default_gridsize = 5
         self._gridsize = self._default_gridsize
-        self._cellsize = min([self._width, self._height]) - self._frame_pad
         self._font = "Verdana" if "Verdana" in font.families() else None
 
         self.window.config(background=COLORS['WINDOW_BACKGROUND'])
@@ -24,24 +32,81 @@ class Application:
         self.window.resizable(False, False)
         try: self.window.tk.call('wm', 'iconphoto', self.window._w, PhotoImage(file=f'assets{sep}icon.png'))
         except: pass
-        self.setup_window()
+        self.cells = [[Cell() for _ in range(self._gridsize)] for _ in range(self._gridsize)]
+        self.draw_window()
         self.window.after(500, self.window.focus_force)
     
     def run(self):
         self.window.mainloop()
     
-    def setup_window(self):
-        self._mainframe = Frame(self.window, width=self._width, height=self._height, borderwidth=1)
-        self.cells = [[str(i+j) for i in range(self._gridsize)] for j in range(self._gridsize)]
-        self.display_cells()
-        self._mainframe.pack(padx=self._frame_pad, pady=self._frame_pad)
+    def draw_window(self):
+        try: 
+            self._mainframe.destroy()
+            self._buttonframe.destroy()
+        except: 
+            pass
+        self._mainframe = Frame(self.window, width=self._width, height=3*self._height//4)
+        self._buttonframe = Frame(self.window, width=self._width, height=self._height//4)
+        self.update_cell_display()
+        self.create_menubuttons()
+        self._mainframe.place(x=self._frame_pad, y=self._frame_pad)
+        self._buttonframe.place(
+            x=self._frame_pad + (self._mainframe['width']//4), 
+            y=self._mainframe['height'] + (self._frame_pad*2)
+            )
     
-    def display_cells(self):
+    def update_cell_display(self):
+        cellwidth = (self._mainframe['width'] - (2*self._frame_pad)) // self._gridsize
+        cellheight = (self._mainframe['height'] - (2*self._frame_pad)) // self._gridsize
         for rn, r in enumerate(self.cells):
-            for cn, s in enumerate(r):
-                btn = Button(self._mainframe, text=s, width=5, height=5, command=partial(print, cn, rn, s))
-                btn.grid(row=cn, column=rn)
-                
+            for cn, c in enumerate(r):
+                cbtn = Button(
+                    self._mainframe, 
+                    text=c.content,
+                    command=partial(self.callbacks.gridbutton, self, cn, rn)
+                    )
+                cbtn.place(x=(rn*cellwidth), y=(cn*cellheight), width=cellwidth, height=cellheight)
+
+    
+    def create_menubuttons(self):
+        btn_export   = ttkButton(self._buttonframe, text="Export to MIDI",      command=partial(self.callbacks.export,              self))
+        btn_increase = ttkButton(self._buttonframe, text="Increase grid size",  command=partial(self.callbacks.increase_gridsize,   self))
+        btn_decrease = ttkButton(self._buttonframe, text="Decrease grid size",  command=partial(self.callbacks.decrease_gridsize,   self))
+        btn_settings = ttkButton(self._buttonframe, text="Settings",            command=partial(self.callbacks.settings,            self))
+
+        btn_increase.grid(row=1, column=0,               sticky='nsew')
+        btn_decrease.grid(row=1, column=1,               sticky='nsew')
+        btn_export.grid  (row=0, column=0, columnspan=2, sticky='nsew')
+        btn_settings.grid(row=2, column=0, columnspan=2, sticky='nsew')
+
+    class callbacks:
+        def gridbutton(self, x, y):
+            c = self.cells[y][x]
+
+            self.update_cell_display()
+            
+        
+        def export(self):
+            pass
+        
+        def increase_gridsize(self):
+            if self._gridsize < self._maximum_gridsize:
+                self._gridsize += 1
+                arr = np.array(self.cells, dtype=Cell)
+                arr = np.resize(arr, [self._gridsize, self._gridsize])
+                self.cells = np.where(arr==0, Cell(), arr)
+                self.draw_window()
+        
+        def decrease_gridsize(self):
+            if self._gridsize > self._minimum_gridsize:
+                self._gridsize -= 1
+                arr = np.array(self.cells, dtype=Cell)
+                arr = np.resize(arr, [self._gridsize, self._gridsize])
+                self.cells = np.where(arr==0, Cell(), arr)
+                self.draw_window()
+        
+        def settings(self):
+            pass
 
 if __name__ == "__main__":
             
@@ -56,7 +121,6 @@ if __name__ == "__main__":
             "TEXTCOLOR": 'systemTextColor'
         }
         
-
     __version__ = 'v0.1'
 
     try:
