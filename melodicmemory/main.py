6691a82fbed2f6ec9ea77899f9c91af6f40914c7
ChristianLoizou <cloizou777@gmail.com>
2022-03-04 10:42:49 +0000

    Created the simple Tkinter application

--- melodicmemory/main.py
+++ melodicmemory/main.py
@@ -1,12 +1,30 @@
+from os import path, sep
+from tkinter import *
+from tkinter import messagebox
+
+
+class Application(Tk):
+    def __init__(self, version, *args, **kwargs):
+        super(Application, self).__init__(*args, **kwargs)
+        self.version = version
+        self.title(f"MelodicMemory {self.version}")
+        self.geometry('300x300+150+150')
+        self.resizable(False, False)
+        self.tk.call('wm', 'iconphoto', self._w,
+                     PhotoImage(file=f'assets{sep}icon.png'))
+
 
 if __name__ == "__main__":
     __version__ = "v0.1"
 
     try:
         from application_update import execute_update
-        import os
-        if execute_update('melodicmemory', __version__, os.path.basename(__file__)):
+        if execute_update('melodicmemory', __version__, path.basename(__file__)):
             exit()
 
     except ModuleNotFoundError:
         pass
+
+    application = Application(__version__)
+    application.after(500, application.focus_force)
+    application.mainloop()
